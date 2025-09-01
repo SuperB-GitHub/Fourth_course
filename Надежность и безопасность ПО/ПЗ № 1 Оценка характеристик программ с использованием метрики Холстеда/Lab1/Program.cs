9f00201427faac63c4abda54bd29d5db0aa837ed
SuperB-GitHub <mr.covalets2015@yandex.ru>
2025-09-01 08:10:15 +0300

    Add to repo

---
+++ Надежность и безопасность ПО/ПЗ № 1 Оценка характеристик программ с использованием метрики Холстеда/Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Lab1
+{
+    static void Maxmin(ref int x, ref int y)
+    {
+        if (x < y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write($"Введите значение A: ");
+        int A = int.Parse(Console.ReadLine());
+        Console.Write($"Введите значение B: ");
+        int B = int.Parse(Console.ReadLine());
+        Console.Write($"Введите значение C: ");
+        int C = int.Parse(Console.ReadLine());
+
+        Maxmin(ref A, ref B);
+        Maxmin(ref B, ref C);
+        Maxmin(ref A, ref B);
+
+        Console.Write($"\nA<B<C : {A}<{B}<{C}");
+    }
+
+}
---
+++ Надежность и безопасность ПО/ПЗ № 2 Оценка характеристик программ с использованием метрики Джилба/Lab2/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Lab2
+{
+    static void Main()
+    {
+        Console.WriteLine("Число | Квадрат");
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine($"  {i}   |  {Math.Pow(i, 2)}");
+        }
+    }
+}
