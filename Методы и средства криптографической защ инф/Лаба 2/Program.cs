aa82585cae6b0309296258d887cefb80ee480b99
SuperB-GitHub <mr.covalets2015@yandex.ru>
2025-09-03 18:09:31 +0300

    Немного лабы 2 крипты

---
+++ Методы и средства криптографической защ инф/Лаба 2/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+        while (true)
+        {
+            Console.WriteLine($"Выберите задание:\n 1 - Шифр Цезаря со сдвигом\n 2 - Афинный шифр Цезаря\n 3 - Шифр Цезаря со словом\n 4 - Шифр Трисемуса\n");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.D1)
+            {
+                Console.Clear();
+
+                string text = "МЫ ДОЛЖНЫ ПРИЗНАТЬ ОЧЕВИДНОЕ: ПОНИМАЮТ ЛИШЬ ТЕ, КТО ХОЧЕТ ПОНЯТЬ";
+                int shift = 7;
+
+                Console.WriteLine($"Выберите действие:\n 1 - Шифрование\n 2 - Дешифрование");
+                key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Текст: {text}\n");
+                    string encrypted = CaesarOne(text, shift);
+                    Console.WriteLine($"Зашифрованный текст: {encrypted}\n");
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    string encrypted = CaesarOne(text, shift);
+                    string decrypted = CaesarOne(encrypted, -shift);
+                    Console.WriteLine($"Зашифрованный текст: {encrypted}\n");
+                    Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (key.Key == ConsoleKey.D2)
+            {
+                Console.Clear();
+                //string text = "АЕРУТНСВЧ";
+                //int[,] square = {
+                //    {2, 7, 6},
+                //    {9, 5, 1},
+                //    {4, 3, 8}
+                //};
+                Console.WriteLine($"Выберите действие:\n 1 - Шифрование\n 2 - Дешифрование");
+                key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    ////string decrypted = DecryptTwo(text, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    //string decrypted = DecryptTwo(text, square);
+                    //string encrypted = EncryptTwo(decrypted, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                    //Console.WriteLine($"Зашифрованный текст: {encrypted}\n");
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            else if (key.Key == ConsoleKey.D3)
+            {
+                Console.Clear();
+                //string text = "АЕРУТНСВЧ";
+                //int[,] square = {
+                //    {2, 7, 6},
+                //    {9, 5, 1},
+                //    {4, 3, 8}
+                //};
+                Console.WriteLine($"Выберите действие:\n 1 - Шифрование\n 2 - Дешифрование");
+                key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    ////string decrypted = DecryptTwo(text, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    //string decrypted = DecryptTwo(text, square);
+                    //string encrypted = EncryptTwo(decrypted, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                    //Console.WriteLine($"Зашифрованный текст: {encrypted}\n");
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            else if (key.Key == ConsoleKey.D4)
+            {
+                Console.Clear();
+                //string text = "АЕРУТНСВЧ";
+                //int[,] square = {
+                //    {2, 7, 6},
+                //    {9, 5, 1},
+                //    {4, 3, 8}
+                //};
+                Console.WriteLine($"Выберите действие:\n 1 - Шифрование\n 2 - Дешифрование");
+                key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    ////string decrypted = DecryptTwo(text, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    //string decrypted = DecryptTwo(text, square);
+                    //string encrypted = EncryptTwo(decrypted, square);
+                    //Console.WriteLine($"Расшифрованный текст: {decrypted}\n");
+                    //Console.WriteLine($"Зашифрованный текст: {encrypted}\n");
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    public static string CaesarOne(string text, int shift)
+    {
+        string Alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in text)
+        {
+            if (char.IsLetter(c))
+            {
+                int index = Alphabet.IndexOf(c);
+
+                if (index != -1)
+                {
+                    int newIndex = (index + shift + Alphabet.Length) % Alphabet.Length;
+                    char encrypted = Alphabet[newIndex];
+
+                    result.Append(encrypted);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+
+
+}
