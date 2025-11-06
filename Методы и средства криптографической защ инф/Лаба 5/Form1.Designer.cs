bc7312c032939389872df85e3bae8ac5a490e44c
SuperB-GitHub <mr.covalets2015@yandex.ru>
2025-11-06 23:53:31 +0300

    Начата 5 лаба

---
+++ Методы и средства криптографической защ инф/Лаба 5/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Лаба_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BTN_Encrypt = new System.Windows.Forms.Button();
+            this.RTB_InputOT = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.RTB_Crypted = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(984, 561);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(976, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Шифрование";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(976, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Расшифрование";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Encrypt, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(970, 529);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(964, 126);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Таблица для Шифрования/Расшифрования";
+            // 
+            // BTN_Encrypt
+            // 
+            this.BTN_Encrypt.BackColor = System.Drawing.Color.SteelBlue;
+            this.BTN_Encrypt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BTN_Encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTN_Encrypt.Location = new System.Drawing.Point(3, 267);
+            this.BTN_Encrypt.Name = "BTN_Encrypt";
+            this.BTN_Encrypt.Size = new System.Drawing.Size(964, 126);
+            this.BTN_Encrypt.TabIndex = 4;
+            this.BTN_Encrypt.Text = "Зашифровать сообщение";
+            this.BTN_Encrypt.UseVisualStyleBackColor = false;
+            this.BTN_Encrypt.Click += new System.EventHandler(this.BTN_Encrypt_Click);
+            // 
+            // RTB_InputOT
+            // 
+            this.RTB_InputOT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RTB_InputOT.Location = new System.Drawing.Point(3, 25);
+            this.RTB_InputOT.Name = "RTB_InputOT";
+            this.RTB_InputOT.Size = new System.Drawing.Size(958, 98);
+            this.RTB_InputOT.TabIndex = 0;
+            this.RTB_InputOT.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RTB_InputOT);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(964, 126);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Открытый текст";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.RTB_Crypted);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(3, 399);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(964, 127);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Шифртекст";
+            // 
+            // RTB_Crypted
+            // 
+            this.RTB_Crypted.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RTB_Crypted.Location = new System.Drawing.Point(3, 25);
+            this.RTB_Crypted.Name = "RTB_Crypted";
+            this.RTB_Crypted.Size = new System.Drawing.Size(958, 99);
+            this.RTB_Crypted.TabIndex = 0;
+            this.RTB_Crypted.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа 5";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BTN_Encrypt;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox RTB_Crypted;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox RTB_InputOT;
+    }
+}
+
---
+++ Методы и средства криптографической защ инф/Лаба 5/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лаба_5
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Dictionary<char, (int start, int end)> CipherMap = new Dictionary<char, (int, int)>()
+        {
+            { ' ', (0, 144) },
+            { 'О', (145, 239) },
+            { 'Е', (240, 313) },
+            { 'А', (314, 377) },
+            { 'И', (378, 441) },
+            { 'Т', (442, 497) },
+            { 'Н', (498, 552) },
+            { 'С', (553, 598) },
+            { 'Р', (599, 639) },
+            { 'В', (640, 678) },
+            { 'Л', (679, 714) },
+            { 'К', (715, 743) },
+            { 'М', (744, 769) },
+            { 'Д', (770, 795) },
+            { 'П', (796, 819) },
+            { 'У', (820, 840) },
+            { 'Я', (841, 859) },
+            { 'Ы', (860, 875) },
+            { 'З', (876, 890) },
+            { 'Ь', (891, 905) },
+            { 'Ъ', (891, 905) },
+            { 'Б', (906, 920) },
+            { 'Г', (921, 934) },
+            { 'Ч', (935, 947) },
+            { 'Й', (948, 957) },
+            { 'Х', (958, 966) },
+            { 'Ж', (967, 974) },
+            { 'Ю', (975, 981) },
+            { 'Ш', (982, 987) },
+            { 'Ц', (988, 991) },
+            { 'Щ', (992, 994) },
+            { 'Э', (995, 997) },
+            { 'Ф', (998, 999) }
+        };
+        private static readonly Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Encrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string input = RTB_InputOT.Text;
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    RTB_Crypted.Text = "";
+                    return;
+                }
+
+                string encrypted = Encrypt(input);
+                RTB_Crypted.Text = encrypted;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при шифровании:\n{ex.Message}",
+                                "Ошибка",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        private string Encrypt(string text)
+        {
+            var result = new StringBuilder();
+            string upperText = text.ToUpperInvariant();
+
+            foreach (char c in upperText)
+            {
+                if (CipherMap.TryGetValue(c, out var range))
+                {
+                    int code = _random.Next(range.start, range.end + 1);
+                    if (result.Length > 0)
+                        result.Append(' ');
+                    result.Append(code.ToString("D3"));
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
