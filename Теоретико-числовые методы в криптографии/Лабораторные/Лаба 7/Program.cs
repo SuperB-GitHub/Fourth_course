8204e021a39e218a352f5095c3f57e84745d3887
SuperB-GitHub <mr.covalets2015@yandex.ru>
2025-12-01 01:05:02 +0300

    Добавил вводов

--- Теоретико-числовые методы в криптографии/Лабораторные/Лаба 7/Program.cs
+++ Теоретико-числовые методы в криптографии/Лабораторные/Лаба 7/Program.cs
@@ -39,7 +39,8 @@ class Program
             else if (key.Key == ConsoleKey.D3)
             {
                 Console.Clear();
-
+                Console.WriteLine($"Решить сравнение первой степени с помощью непрерывных дробей\n");
+                InputThird(out int a, out int b, out int p);
 
                 contin = OutputEnd();
 
@@ -52,6 +53,7 @@ class Program
         }
     }
 
+    // Ввод/вывод информации
     static void InputFirst(out int a, out int b)
     {
         Console.Write($"Введите значение для а: ");
@@ -77,18 +79,41 @@ class Program
         }
 
         Console.Write($"Введите значения коэффициентов через пробел: ");
-        while ()
+        string input = Console.ReadLine()!;
+        while (string.IsNullOrWhiteSpace(input))
         {
-
+            Console.Write("Некорректный ввод. Введите целые числа: ");
+            input = Console.ReadLine()!;
         }
 
-        string input = Console.ReadLine();
         qi = input.Split(' ')
             .Where(x => !string.IsNullOrWhiteSpace(x))
             .Select(int.Parse)
             .ToList();
 
         Console.WriteLine($"\nПолученная дробь: [{a}; {string.Join(", ", qi)}]");
+    }
+    static void InputThird(out int a, out int b, out int p)
+    {
+        Console.Write($"Введите значение для а: ");
+        while (!int.TryParse(Console.ReadLine(), out a))
+        {
+            Console.Write("Некорректный ввод. Введите целое число: ");
+        }
+
+        Console.Write($"Введите значение для b: ");
+        while (!int.TryParse(Console.ReadLine(), out b))
+        {
+            Console.Write("Некорректный ввод. Введите целое число: ");
+        }
+
+        Console.Write($"Введите значение для p: ");
+        while (!int.TryParse(Console.ReadLine(), out p))
+        {
+            Console.Write("Некорректный ввод. Введите простое целое число: ");
+
+        }
+        Console.WriteLine($"\nПолученное выражение: {a}x ≡ {b}(mod {p})");
     }
     static bool OutputEnd()
     {
@@ -97,33 +122,77 @@ class Program
         if (key.Key != ConsoleKey.Enter) { return false; }
         return true;
     }
+    //public static void PrintFraction(List<int> qi)
+    //{
+    //    string pm = " + ";
+    //    int lenpm = pm.Length;
+
+    //    string first = $"{new string(' ', qi[0].ToString().Length)}{new string(' ', lenpm)}1";
+    //    Console.WriteLine("\n" + first);
+
+    //    for (int i = 1; i < qi.Count; i++)
+    //    {
+    //        if (i == qi.Count - 1)
+    //        {
+    //            Console.WriteLine($"{new string(' ', (i - 1) * (first.Length - 1))}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}");
+    //            Console.WriteLine($"{new string(' ', (i) * (first.Length - 1))}{qi[i]}");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine($"{new string(' ', (i - 1) * (first.Length - 1))}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}{new string(' ', lenpm)}1");
+    //        }
+    //    }
+    //}
     public static void PrintFraction(List<int> qi)
     {
+        if (qi == null || qi.Count == 0) return;
+
         string pm = " + ";
         int lenpm = pm.Length;
 
-        string first = $"{new string(' ', qi[0].ToString().Length)}{new string(' ', lenpm)}1";
-        Console.WriteLine("\n" + first);
+        // Строим структуру дроби в памяти для расчета позиций
+        List<string> lines = new List<string>();
 
+        // Первая строка
+        lines.Add($"{new string(' ', qi[0].ToString().Length)}{new string(' ', lenpm)}1");
+
+        // Последующие строки
         for (int i = 1; i < qi.Count; i++)
         {
+            int currentIndent = 0;
+
+            // Рассчитываем отступ на основе предыдущих коэффициентов
+            for (int j = 0; j < i - 1; j++)
+            {
+                currentIndent += qi[j].ToString().Length + lenpm;
+            }
+
             if (i == qi.Count - 1)
             {
-                Console.WriteLine($"{new string(' ', (i - 1) * (first.Length - 1))}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}");
-                Console.WriteLine($"{new string(' ', (i) * (first.Length - 1))}{qi[i]}");
+                // Последний элемент - числитель и знаменатель
+                string numLine = $"{new string(' ', currentIndent)}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}";
+                string denomLine = $"{new string(' ', currentIndent + qi[i - 1].ToString().Length + lenpm)}{qi[i]}";
+
+                lines.Add(numLine);
+                lines.Add(denomLine);
             }
             else
             {
-                Console.WriteLine($"{new string(' ', (i - 1) * (first.Length - 1))}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}{new string(' ', lenpm)}1");
+                // Промежуточный элемент
+                string line = $"{new string(' ', currentIndent)}{qi[i - 1]}{pm}{new string('-', qi[i].ToString().Length)}{new string(' ', lenpm)}1";
+                lines.Add(line);
             }
-
         }
 
-
-
-
+        // Выводим все строки
+        Console.WriteLine();
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
     }
 
+    // Работа алгоритмов
     static void EuclidAlg(ref int a, ref int b)
     {
         List<int> ai = new List<int> { a };
@@ -131,27 +200,26 @@ class Program
         List<int> qi = new List<int> { };
         List<int> ri = new List<int> { };
 
-        int r = ai[0] - (ai[0] / bi[0]) * bi[0];
         int i = 0;
-        while (r != 0)
+        int r;
+
+        do
         {
-            int q = ai[i] / bi[i];
+            int q = (int)Math.Floor((double)ai[i] / bi[i]);
             qi.Add(q);
             r = ai[i] - q * bi[i];
             ri.Add(r);
+
+            Console.WriteLine($"{ai[i]} = {q} * {bi[i]} + {r} |:{bi[i]}     {ai[i]}/{bi[i]} = {q} + {r}/{bi[i]}");
+
             ai.Add(bi[i]);
             bi.Add(r);
-            Console.WriteLine($"{ai[i]} = {qi[i]} * {bi[i]} + {ri[i]} |:{bi[i]}     {ai[i]}/{bi[i]} = {qi[i]} + {ri[i]}/{bi[i]}");
             i++;
-        }
+
+        } while (r != 0);
 
         Console.Write($"\n{ai[0]}/{bi[0]} = ");
         PrintFraction(qi);
-        //for (int _ = 0; _ < qi.Count-1; _++)
-        //{
-        //    Console.Write($"{qi[_]} + 1 / ");
-        //}
-        //Console.Write(qi[qi.Count - 1]);
 
         Console.Write($"\n{ai[0]}/{bi[0]} = [{qi[0]}; ");
         for (int _ = 1; _ < qi.Count; _++)
@@ -165,9 +233,5 @@ class Program
                 Console.Write($"1/{qi[_]}, ");
             }
         }
-
-        
-
     }
-
 }
