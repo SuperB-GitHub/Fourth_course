ad52c372483fda25ccb3ab01b28c8fe46ea5a648
SuperB-GitHub <mr.covalets2015@yandex.ru>
2026-03-09 18:03:26 +0300

    Добавление 3 лабы МАГК

--- 8 семестр/Методы алгебраической геометрии/WinFormsApp1/Form1.Designer.cs
+++ 8 семестр/Методы алгебраической геометрии/WinFormsApp1/Form1.Designer.cs
@@ -1,39 +1,141 @@
 ﻿namespace WinFormsApp1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing && (components != null))
-        //    {
-        //        components.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        //private void InitializeComponent()
-        //{
-        //    components = new System.ComponentModel.Container();
-        //    AutoScaleMode = AutoScaleMode.Font;
-        //    ClientSize = new Size(800, 450);
-        //    Text = "Form1";
-        //}
+        private void InitializeComponent()
+        {
+            tbA = new TextBox();
+            tbB = new TextBox();
+            btnGenerate = new Button();
+            graphPanel = new Panel();
+            lblResult = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            labelResult = new Label();
+            SuspendLayout();
+            // 
+            // tbA
+            // 
+            tbA.Location = new Point(57, 15);
+            tbA.Margin = new Padding(3, 2, 3, 2);
+            tbA.Name = "tbA";
+            tbA.Size = new Size(53, 23);
+            tbA.TabIndex = 1;
+            tbA.Text = "-3";
+            // 
+            // tbB
+            // 
+            tbB.Location = new Point(151, 16);
+            tbB.Margin = new Padding(3, 2, 3, 2);
+            tbB.Name = "tbB";
+            tbB.Size = new Size(53, 23);
+            tbB.TabIndex = 3;
+            tbB.Text = "1";
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(231, 14);
+            btnGenerate.Margin = new Padding(3, 2, 3, 2);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(172, 29);
+            btnGenerate.TabIndex = 4;
+            btnGenerate.Text = "Построить кривую";
+            btnGenerate.Click += BtnGenerate_Click;
+            // 
+            // graphPanel
+            // 
+            graphPanel.BackColor = Color.White;
+            graphPanel.BorderStyle = BorderStyle.FixedSingle;
+            graphPanel.Location = new Point(18, 75);
+            graphPanel.Margin = new Padding(3, 2, 3, 2);
+            graphPanel.Name = "graphPanel";
+            graphPanel.Size = new Size(1254, 675);
+            graphPanel.TabIndex = 6;
+            graphPanel.Paint += GraphPanel_Paint;
+            // 
+            // lblResult
+            // 
+            lblResult.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            lblResult.Location = new Point(18, 45);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(109, 22);
+            lblResult.TabIndex = 5;
+            lblResult.Text = "Результат:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(18, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 21);
+            label1.TabIndex = 7;
+            label1.Text = "a = ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(116, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 21);
+            label2.TabIndex = 8;
+            label2.Text = "b = ";
+            // 
+            // labelResult
+            // 
+            labelResult.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelResult.Location = new Point(116, 45);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(676, 22);
+            labelResult.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1284, 761);
+            Controls.Add(labelResult);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tbA);
+            Controls.Add(tbB);
+            Controls.Add(btnGenerate);
+            Controls.Add(lblResult);
+            Controls.Add(graphPanel);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Эллиптические кривые над полем вещественных чисел";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Panel graphPanel;
+        private System.Windows.Forms.Label lblResult;
+        private Label label1;
+        private Label label2;
+        private Label labelResult;
     }
 }
--- 8 семестр/Методы алгебраической геометрии/WinFormsApp1/Form1.cs
+++ 8 семестр/Методы алгебраической геометрии/WinFormsApp1/Form1.cs
@@ -7,38 +7,11 @@ namespace WinFormsApp1
 {
     public partial class MainForm : Form
     {
-        private TextBox tbA, tbB;
-        private Button btnGenerate;
-        private Panel graphPanel;
-        private Label lblResult;
         private float a, b;
 
         public MainForm()
         {
             InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            this.Text = "Эллиптические кривые над полем вещественных чисел";
-            this.Size = new Size(900, 700);
-            this.StartPosition = FormStartPosition.CenterScreen;
-
-            Label lblA = new Label() { Text = "a:", Location = new Point(20, 20), Size = new Size(30, 25) };
-            tbA = new TextBox() { Location = new Point(50, 20), Size = new Size(60, 25), Text = "-3" };
-
-            Label lblB = new Label() { Text = "b:", Location = new Point(130, 20), Size = new Size(30, 25) };
-            tbB = new TextBox() { Location = new Point(160, 20), Size = new Size(60, 25), Text = "1" };
-
-            btnGenerate = new Button() { Text = "Построить кривую", Location = new Point(240, 18), Size = new Size(120, 30) };
-
-            lblResult = new Label() { Text = "Результат:", Location = new Point(20, 60), Size = new Size(850, 30), Font = new Font("Arial", 10, FontStyle.Bold) };
-
-            graphPanel = new Panel() { Location = new Point(20, 100), Size = new Size(850, 550), BorderStyle = BorderStyle.FixedSingle, BackColor = Color.White };
-            graphPanel.Paint += GraphPanel_Paint;
-
-            this.Controls.AddRange(new Control[] { lblA, tbA, lblB, tbB, btnGenerate, lblResult, graphPanel });
-            btnGenerate.Click += BtnGenerate_Click;
         }
 
         private void BtnGenerate_Click(object sender, EventArgs e)
@@ -59,21 +32,20 @@ namespace WinFormsApp1
         private void CheckSingularity()
         {
             double D = 4 * Math.Pow(a, 3) + 27 * Math.Pow(b, 2);
-
             if (Math.Abs(D) < 1e-10)
             {
-                lblResult.Text = $"СИНГУЛЯРНАЯ кривая (дискриминант = 0)";
-                lblResult.ForeColor = Color.Red;
+                labelResult.Text = $"Кривая сингулярна (дискриминант = 0)";
+                labelResult.ForeColor = Color.Red;
 
                 if (Math.Abs(a) < 1e-10 && Math.Abs(b) < 1e-10)
-                    lblResult.Text += " - КАСП (точка возврата)";
+                    labelResult.Text += " - Точка возврата";
                 else
-                    lblResult.Text += " - САМОПЕРЕСЕЧЕНИЕ";
+                    labelResult.Text += " - Самопересечение";
             }
             else
             {
-                lblResult.Text = $"НЕСИНГУЛЯРНАЯ кривая (дискриминант = {D:F4})";
-                lblResult.ForeColor = Color.Green;
+                labelResult.Text = $"Кривая не сингулярна (дискриминант = {D:F4})";
+                labelResult.ForeColor = Color.Green;
             }
         }
 
@@ -87,7 +59,6 @@ namespace WinFormsApp1
             int cx = w / 2;
             int cy = h / 2;
 
-            // Рисуем оси
             using (Pen pen = new Pen(Color.Black, 1))
             {
                 g.DrawLine(pen, cx, 0, cx, h);
@@ -99,16 +70,14 @@ namespace WinFormsApp1
 
             float scale = 30f;
 
-            // Находим интервалы, где функция определена
-            List<Interval> intervals = FindDefinedIntervals(-10f, 10f, 0.00001f);
+            var intervals = FindDefinedIntervals(-10f, 10f, 0.00001f);
 
             using (Pen curvePen = new Pen(Color.Blue, 2))
             {
                 foreach (var interval in intervals)
                 {
-                    // Рисуем верхнюю и нижнюю ветвь
-                    List<PointF> upperPoints = new List<PointF>();
-                    List<PointF> lowerPoints = new List<PointF>();
+                    List<PointF> upper = new List<PointF>();
+                    List<PointF> lower = new List<PointF>();
 
                     for (float x = interval.Start; x <= interval.End; x += 0.0001f)
                     {
@@ -119,22 +88,17 @@ namespace WinFormsApp1
                         float screenY_up = cy - y * scale;
                         float screenY_down = cy + y * scale;
 
-                        if (screenX >= 0 && screenX <= w)
-                        {
-                            // Верхняя ветвь (всегда добавляем, даже если y=0)
-                            if (screenY_up >= 0 && screenY_up <= h)
-                                upperPoints.Add(new PointF(screenX, screenY_up));
+                        if (screenX < 0 || screenX > w) continue;
 
-                            // Нижняя ветвь - добавляем всегда, но для y=0 точки будут совпадать с верхними
-                            if (screenY_down >= 0 && screenY_down <= h)
-                                lowerPoints.Add(new PointF(screenX, screenY_down));
-                        }
+                        if (screenY_up >= 0 && screenY_up <= h)
+                            upper.Add(new PointF(screenX, screenY_up));
+
+                        if (screenY_down >= 0 && screenY_down <= h)
+                            lower.Add(new PointF(screenX, screenY_down));
                     }
 
-                    if (upperPoints.Count > 1)
-                        g.DrawLines(curvePen, upperPoints.ToArray());
-                    if (lowerPoints.Count > 1)
-                        g.DrawLines(curvePen, lowerPoints.ToArray());
+                    if (upper.Count > 1) g.DrawLines(curvePen, upper.ToArray());
+                    if (lower.Count > 1) g.DrawLines(curvePen, lower.ToArray());
                 }
             }
 
@@ -143,34 +107,29 @@ namespace WinFormsApp1
 
         private List<Interval> FindDefinedIntervals(float start, float end, float step)
         {
-            List<Interval> intervals = new List<Interval>();
+            var intervals = new List<Interval>();
             bool inInterval = false;
             float intervalStart = 0;
 
             for (float x = start; x <= end; x += step)
             {
-                float val = (float)(Math.Pow(x, 3) + a * x + b);
-                bool isDefined = val >= 0;
+                float val = (float)(x * x * x + a * x + b);
+                bool defined = val >= 0;
 
-                if (isDefined && !inInterval)
+                if (defined && !inInterval)
                 {
-                    // Начало нового интервала
                     inInterval = true;
                     intervalStart = x;
                 }
-                else if (!isDefined && inInterval)
+                else if (!defined && inInterval)
                 {
-                    // Конец интервала
                     inInterval = false;
                     intervals.Add(new Interval(intervalStart, x - step));
                 }
             }
 
-            // Если закончили в интервале
             if (inInterval)
-            {
                 intervals.Add(new Interval(intervalStart, end));
-            }
 
             return intervals;
         }
@@ -181,42 +140,37 @@ namespace WinFormsApp1
             {
                 float x1 = -(float)Math.Sqrt(-a / 3);
                 float x2 = (float)Math.Sqrt(-a / 3);
-
                 CheckPoint(g, cx, cy, scale, x1);
                 CheckPoint(g, cx, cy, scale, x2);
             }
 
             if (Math.Abs(a) < 0.001f && Math.Abs(b) < 0.001f)
             {
-                float screenX = cx;
-                float screenY = cy;
-                g.FillEllipse(Brushes.Red, screenX - 5, screenY - 5, 10, 10);
-                g.DrawString("КАСП", new Font("Arial", 8), Brushes.Red, screenX + 10, screenY - 15);
+                float sx = cx;
+                float sy = cy;
+                g.FillEllipse(Brushes.Red, sx - 5, sy - 5, 10, 10);
+                g.DrawString("Точка возврата", new Font("Arial", 9), Brushes.Red, sx + 10, sy - 15);
             }
         }
 
         private void CheckPoint(Graphics g, int cx, int cy, float scale, float x)
         {
             float val = (float)(Math.Pow(x, 3) + a * x + b);
-
-            if (Math.Abs(val) < 0.01f && val >= 0)
+            if (val >= -0.01f && val <= 0.01f)   // ≈ 0
             {
-                float screenX = cx + x * scale;
-                float screenY = cy;
+                float sx = cx + x * scale;
+                if (sx < 0 || sx > graphPanel.Width) return;
 
-                if (screenX >= 0 && screenX <= graphPanel.Width)
-                {
-                    g.FillEllipse(Brushes.Red, screenX - 5, screenY - 5, 10, 10);
-                    g.DrawString("Особая точка", new Font("Arial", 7), Brushes.Red, screenX + 10, screenY - 10);
-                }
+                float sy = cy;
+                g.FillEllipse(Brushes.Red, sx - 5, sy - 5, 10, 10);
+                g.DrawString("Особая точка", new Font("Arial", 9), Brushes.Red, sx + 10, sy - 10);
             }
         }
 
-        // Вспомогательный класс для хранения интервалов
         private class Interval
         {
-            public float Start { get; set; }
-            public float End { get; set; }
+            public float Start { get; }
+            public float End { get; }
 
             public Interval(float start, float end)
             {
@@ -226,4 +180,3 @@ namespace WinFormsApp1
         }
     }
 }
-
